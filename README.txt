ff251d1ab4d06ae54a5432636fa2121a411b59dc
Nicolas RICHARD <nicolas.richard1@etu.univ-nantes.fr>
2017-04-04 09:34:01 +0200

    commentaires

---
+++ README.txt
@@ -0,0 +1,6 @@
+1) Initialiser la base de données (changer les identifiants si nécessaire)
+python3 src/modele/initBD.py
+2) Mise à jour des données et remplissage des tables
+python3 src/modele/initData.py
+3) Lancer le serveur
+python3 src/controlleur/serverBottle.py
--- src/controlleur/serverBottle.py
+++ src/controlleur/serverBottle.py
@@ -1,21 +1,17 @@
-from bottle import *
-import sys
 import json
+#Bibliothèques du projet
+import sys
 path ="/hometu/etudiants/r/i/E155059S/PycharmProjects/ProdLog/src/controller"
 sys.path.append(path)
 from dao import *
+from bottle import *
 
+#Accueil du site
 @route('/')
 def index():
     return static_file("site.html", root='./vue/html')
 
-@route('/rechercheBD', method='GET')
-def resultat():
-    commune = request.query.commune
-    activite = request.query.activite
-    niveau = request.query.niveau
-    return json.dumps(find(commune,activite,niveau))
-
+#Ressources utilisées
 @route('/img/<filename>')
 def routeIMG(filename):
     return static_file(filename, root='./vue/img')
@@ -28,14 +24,7 @@ def routeCSS(filename):
 def routeCSS(filename):
     return static_file(filename, root='./vue/script')
 
-@error(404)
-def error404(error):
-    return static_file("404.html", root='./vue/html')
-
-@error(500)
-def error500(error):
-    return "On n'a pas prévu ce cas, désolé, vous avez cassé notre code..."
-
+#Requêtes AJAX
 @route("/listeCommunes")
 def getListeCommunes():
     return json.dumps(listeCommunes())
@@ -48,5 +37,21 @@ def getListeActivites():
 def getListeNiveaux():
     return json.dumps(listeNiveaux())
 
+@route('/rechercheBD', method='GET')
+def resultat():
+    commune = request.query.commune
+    activite = request.query.activite
+    niveau = request.query.niveau
+    return json.dumps(find(commune,activite,niveau))
 
-run(host='localhost', port=8666, debug=True)
+#Erreurs
+@error(404)
+def error404(error):
+    return static_file("404.html", root='./vue/html')
+
+@error(500)
+def error500(error):
+    return "On n'a pas prévu ce cas, désolé, vous avez cassé notre code..."
+
+#Lancement
+run(host='0.0.0.0', port=8666, debug=True)
